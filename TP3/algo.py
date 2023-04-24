744039a7bda8f13bf5a676026c835e57fa6cb9c4
Jeperreault <jeremy.perreault@polymtl.ca>
2023-04-24 16:17:30 -0400

    Version 3 de l'algorithme

--- TP3/algo.py
+++ TP3/algo.py
@@ -1,16 +1,16 @@
-import numpy as np 
+import numpy as np
 import math
 from itertools import combinations
 import random
+
 
 def readFile(file):
     n = 0
     m = 0
     k = 0
     subset = []
-    #distances = []
     distances = []
-    enclo_sizes = {}
+    enclosure_sizes = {}
     with open(file, "r") as f:
         lines = f.readlines()
         for i in range(0, len(lines)):
@@ -18,279 +18,259 @@ def readFile(file):
                 n, m, k = map(int, lines[i].split())
             elif i == 1:
                 subset = list(map(int, lines[i].strip().split()))
-            elif i > 1 and i < n + 2 :
-                index = i-2
-                #TODO maybe sort enlcloses here
-                enclo_sizes[index]= (int(lines[i].strip()))
+            elif i > 1 and i < n + 2:
+                index = i - 2
+                # TODO maybe sort enclosures here
+                enclosure_sizes[index] = int(lines[i].strip())
             else:
-                distances = np.resize(distances, (n,n))
+                distances = np.resize(distances, (n, n))
                 index = i - n - 2
-                distances[index,:]= lines[i].strip().split()
-                #distances.append(list(map(int, lines[i].strip().split())))
+                distances[index, :] = lines[i].strip().split()
+                # distances.append(list(map(int, lines[i].strip().split())))
     # print(n, m, k)
     # print(subset)
-    # print(enclo_sizes)
+    # print(enclosure_sizes)
     # print(distances)
-    return n, m , k, subset,enclo_sizes, distances
-   
+    return n, m, k, subset, enclosure_sizes, distances
+
 
 def distance(x1, y1, x2, y2):
-    return abs(x2-x1)+abs(y2-y1)
+    return abs(x2 - x1) + abs(y2 - y1)
+
 
 def sort_all(sizes):
-    #https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
-    enclo_sizes_sort = dict(sorted(sizes.items(),key=lambda x:x[1],reverse=True))
-    return enclo_sizes_sort
+    # https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
+    enclosure_sizes_sort = dict(sorted(sizes.items(), key=lambda x: x[1], reverse=True))
+    return enclosure_sizes_sort
 
 
-def sort_subset_by_size_dict(subset,sorted_enclos):
+def sort_subset_by_size_dict(subset, sorted_enclosures):
     sorted_subset = {}
-    #for s in sub :
+    # for s in sub :
     # TODO find a better solution
-    for enclo, size in sorted_enclos.items():
-        if enclo in subset:
-            sorted_subset[enclo] = size
-    return sorted_subset   
-
-def initialize_theme_zone(sorted_subset,nb) : 
-    enclo_0 = list(sorted_subset.keys())[0] # get biggest enclo
-    size_0 = list(sorted_subset.values())[0] # get size of biggest enclo
-
-    size_1 = list(sorted_subset.values())[1] # get  size of second biggest enclo
+    for enclosure, size in sorted_enclosures.items():
+        if enclosure in subset:
+            sorted_subset[enclosure] = size
+    return sorted_subset
 
 
-    row = math.ceil(size_0/nb)
-    col = math.ceil(size_1/nb) + nb
+def initialize_theme_zone(sorted_subset, nb):
+    enclosure_0 = list(sorted_subset.keys())[0]  # get biggest enclosure
+    size_0 = list(sorted_subset.values())[0]  # get size of biggest enclosure
+
+    size_1 = list(sorted_subset.values())[1]  # get  size of second biggest enclosure
+
+    row = math.ceil(size_0 / nb)
+    col = math.ceil(size_1 / nb) + nb
     # print(row,col)
-    zone = np.full((row,col),-1)
+    zone = np.full((row, col), -1)
     # print(zone)
 
-    zone[:,:1]=enclo_0
-    rest = size_0 - row 
-    zone[:rest,1:nb]=enclo_0
-    #print("ZONE : ")
-    #print(zone)
+    zone[:, :1] = enclosure_0
+    rest = size_0 - row
+    zone[:rest, 1:nb] = enclosure_0
+    # print("ZONE : ")
+    # print(zone)
     return zone
 
+
 def create_theme_zone(sorted_subset):
-    
-    nb= 4 # change code after
-    zone = initialize_theme_zone(sorted_subset,nb)
+    nb = 4  # change code after
+    zone = initialize_theme_zone(sorted_subset, nb)
 
-    sub_zone = zone[:,nb:zone.shape[1]]
-  
-    for i, (enclo, size) in enumerate(sorted_subset.items()):
-        
-        if i == 0 : continue # firt enclo is already set in initialize_theme_zone
-        
-        row,col = np.argwhere(sub_zone==-1)[0]
+    sub_zone = zone[:, nb : zone.shape[1]]
+
+    for i, (enclosure, size) in enumerate(sorted_subset.items()):
+        if i == 0:
+            continue  # first enclosure is already set in initialize_theme_zone
+
+        row, col = np.argwhere(sub_zone == -1)[0]
         # print(row, col)
-    
-        
-        rest = size - (sub_zone.shape[1]-col) 
-        sub_zone[row,col:] = enclo
 
-        if(row + 1 >= sub_zone.shape[0] or row + 2 >= sub_zone.shape[0] ):
-            #sub_zone = np.resize(sub_zone, (sub_zone.shape[0]+2,sub_zone.shape[1]))
-            sub_zone = np.pad(sub_zone, ((0, 2), (0, 0)), mode='constant', constant_values=-1)
-            zone = np.pad(zone, ((0, 2), (0, 0)), mode='constant', constant_values=-1)
+        rest = size - (sub_zone.shape[1] - col)
+        sub_zone[row, col:] = enclosure
 
-        sub_zone[row+1,:rest] = enclo
+        if row + 1 >= sub_zone.shape[0] or row + 2 >= sub_zone.shape[0]:
+            # sub_zone = np.resize(sub_zone, (sub_zone.shape[0]+2,sub_zone.shape[1]))
+            sub_zone = np.pad(
+                sub_zone, ((0, 2), (0, 0)), mode="constant", constant_values=-1
+            )
+            zone = np.pad(zone, ((0, 2), (0, 0)), mode="constant", constant_values=-1)
+
+        sub_zone[row + 1, :rest] = enclosure
 
         # if we need more than 2 rows
-        rest-= sub_zone.shape[1]
-        if (rest>0):
-            
-            sub_zone[row+2,:rest] = enclo
+        rest -= sub_zone.shape[1]
+        if rest > 0:
+            sub_zone[row + 2, :rest] = enclosure
 
-    #print(sub_zone)
-    zone[:,nb:zone.shape[1]] = sub_zone 
-    #print(zone)
+    # print(sub_zone)
+    zone[:, nb : zone.shape[1]] = sub_zone
+    # print(zone)
     return zone
 
 
 def compute_both_ways(distances):
-     
     both_ways = np.transpose(distances)
     both_ways = both_ways + distances
-    return both_ways 
+    return both_ways
+
 
 # glouton
-def create_solution(distances,enclo_sizes,theme_zone,subset,n):
-    #size = int(20/2)
-    zone = np.full(theme_zone.shape,-1)
+def create_solution(distances, enclosure_sizes, theme_zone, subset, n):
+    # size = int(20/2)
+    zone = np.full(theme_zone.shape, -1)
     ############################### sol 2 ##################
     copy_distances = np.copy(distances)
-    
+
     placed = list(np.copy(subset))
     to_place = []
-    
-    for enclo in enclo_sizes.keys():
-        if enclo not in placed:
-            to_place.append(enclo)
-    #print(to_place)
 
-    for enclo in placed :
-        copy_distances[enclo,:] = 0
-        copy_distances[:,enclo] = 0
-    #print(copy_distances)
+    for enclosure in enclosure_sizes.keys():
+        if enclosure not in placed:
+            to_place.append(enclosure)
+    # print(to_place)
 
-    enclo_to_place = random.choice(to_place)
-    #print("chosen enclo",enclo_to_place)
+    for enclosure in placed:
+        copy_distances[enclosure, :] = 0
+        copy_distances[:, enclosure] = 0
+    # print(copy_distances)
 
-    while(len(placed) != n ):
-        
+    enclosure_to_place = random.choice(to_place)
+    # print("chosen enclosure",enclosure_to_place)
+
+    while len(placed) != n:
         ###### copied from other funct
-        row,col = np.argwhere(zone==-1)[0]
+        row, col = np.argwhere(zone == -1)[0]
         # print(row, col)
-        #sub_zone[row:5-row, col:5-col] = enclo
-        
-        size = enclo_sizes[enclo_to_place]
-        rest = size - (zone.shape[1]-col) 
-        zone[row,col:] = enclo_to_place
+        # sub_zone[row:5-row, col:5-col] = enclosure
 
-        if(row + 1 >= zone.shape[0] or row + 2 >= zone.shape[0] ):
-            #sub_zone = np.resize(sub_zone, (sub_zone.shape[0]+2,sub_zone.shape[1]))
-            zone = np.pad(zone, ((0, 2), (0, 0)), mode='constant', constant_values=-1)
-         
+        size = enclosure_sizes[enclosure_to_place]
+        rest = size - (zone.shape[1] - col)
+        zone[row, col:] = enclosure_to_place
 
-        zone[row+1,:rest] = enclo_to_place
+        if row + 1 >= zone.shape[0] or row + 2 >= zone.shape[0]:
+            # sub_zone = np.resize(sub_zone, (sub_zone.shape[0]+2,sub_zone.shape[1]))
+            zone = np.pad(zone, ((0, 2), (0, 0)), mode="constant", constant_values=-1)
+
+        zone[row + 1, :rest] = enclosure_to_place
 
         # if we need more than 2 rows
-        rest-= zone.shape[1]
-        if (rest>0):
-            
-            zone[row+2,:rest] = enclo_to_place
+        rest -= zone.shape[1]
+        if rest > 0:
+            zone[row + 2, :rest] = enclosure_to_place
         #####
         # print("ZONE",zone)
 
         # print("DISTANCES",copy_distances)
-        next_enclo_to_place = np.argmax(copy_distances[enclo_to_place])
-        # print('next enclo to place',next_enclo_to_place)
+        next_enclosure_to_place = np.argmax(copy_distances[enclosure_to_place])
+        # print('next enclo to place',next_enclosure_to_place)
 
-        copy_distances[enclo_to_place,:] = 0
-        copy_distances[:,enclo_to_place] = 0
+        copy_distances[enclosure_to_place, :] = 0
+        copy_distances[:, enclosure_to_place] = 0
         # print('UPDATED DISTANCES ',copy_distances)
-        
-        placed.append(enclo_to_place)
-        enclo_to_place = next_enclo_to_place
-       
+
+        placed.append(enclosure_to_place)
+        enclosure_to_place = next_enclosure_to_place
 
     theme_zone = np.flip(theme_zone, axis=0)
     # print(theme_zone)
-    #print(zone)
+    # print(zone)
 
     final_zone = np.concatenate((theme_zone, zone), axis=0)
     # print(final_zone)
-    return(final_zone)
+    return final_zone
 
-  
-#  def create_solution(distances,enclo_sizes,theme_zone,subset): 
+
+#  def create_solution(distances,enclosure_sizes,theme_zone,subset):
 #       ############## sol 1
 #     copy_distances = np.copy(distances)
 #     placed = np.copy(subset)
-#     enclo_to_place = -1
+#     enclosure_to_place = -1
 #     while(not(np.all(copy_distances == 0))):
-    
+
 #         max_index = np.unravel_index(np.argmax(distances), distances.shape)
 #         print(max_index)
 
 #         # if both are already in the theme zone
-#         if(max_index[0] in placed and max_index[1] in placed ) : 
+#         if(max_index[0] in placed and max_index[1] in placed ) :
 #             copy_distances[max_index[0],max_index[1]] = 0
 #             copy_distances[max_index[1],max_index[0]] = 0
 #             continue
-        
+
 #         elif( max_index[0]in placed and max_index[1] not in placed) :
-#             enclo_to_place = max_index[1]
-#             indexes_x = np.where(theme_zone == max_index[0])[0] 
-#             indexes_y = np.where(theme_zone == max_index[0])[1] 
-#             index = (indexes_x[np.argmax(indexes_y)],np.max(indexes_y)+1) 
+#             enclosure_to_place = max_index[1]
+#             indexes_x = np.where(theme_zone == max_index[0])[0]
+#             indexes_y = np.where(theme_zone == max_index[0])[1]
+#             index = (indexes_x[np.argmax(indexes_y)],np.max(indexes_y)+1)
 #             print(index)
-          
-           
-#         elif( max_index[0] not in placed and max_index[1] in placed): 
-#             enclo_to_place = max_index[0]
-        
-#         else: print()# place both // break;  
 
 
-def display_theme_zone(zone,supset):
+#         elif( max_index[0] not in placed and max_index[1] in placed):
+#             enclosure_to_place = max_index[0]
+
+#         else: print()# place both // break;
+
+
+def display_theme_zone(zone, supset):
     coordinates = []
     for i in supset:
-        idx = np.where(zone==i)
+        idx = np.where(zone == i)
         idx = np.column_stack((idx[0], idx[1]))
-        indexes = np.array2string(np.ravel(idx))[1:-1] 
+        indexes = np.array2string(np.ravel(idx))[1:-1]
         # print(idx)
         # print(i)
-        # print(indexes) 
+        # print(indexes)
         ## TODO delete those lines in the end
-        # for row in idx : 
+        # for row in idx :
         #     coordinates.append(tuple(row))
-    
-    
-    #print(coordinates)
-    #return coordinates
-    
-def display_zone(zone,supset):
+
+    # print(coordinates)
+    # return coordinates
+
+
+def display_zone(zone, supset):
     coordinates = []
     for i in supset:
-        idx = np.where(zone==i)
+        idx = np.where(zone == i)
         idx = np.column_stack((idx[0], idx[1]))
-        indexes = np.array2string(np.ravel(idx))[1:-1] 
+        indexes = np.array2string(np.ravel(idx))[1:-1]
         # print(idx)
         # print(i)
-        print(indexes) 
+        print(indexes)
         ## TODO delete those lines in the end
-        # for row in idx : 
+        # for row in idx :
         #     coordinates.append(tuple(row))
-    
-    
-    #print(coordinates)
-    #return coordinates
 
-
+    # print(coordinates)
+    # return coordinates
 
 
 #################### main ##############
 
-n, m , k, subset,enclo_sizes, distances = readFile("ex_n10_m5.txt")
-#n, m , k, subset,enclo_sizes, distances = readFile("n20_m15_V-74779.txt")
-#n, m , k, subset,enclo_sizes, distances = readFile("n100_m50_V-8613404.txt")
-#print(n, m , k, subset,enclo_sizes, distances)
-#print(subset)
-#print(distances)
+n, m, k, subset, enclosure_sizes, distances = readFile("ex_n10_m5.txt")
+# n, m , k, subset,enclosure_sizes, distances = readFile("n20_m15_V-74779.txt")
+# n, m , k, subset,enclosure_sizes, distances = readFile("n100_m50_V-8613404.txt")
+# print(n, m , k, subset,enclosure_sizes, distances)
+# print(subset)
+# print(distances)
 
 
 distances = compute_both_ways(distances)
-#print(distances)
+# print(distances)
 
-sorted_enclo_sizes = sort_all(enclo_sizes)
-print("sorted enclo by size",sorted_enclo_sizes)
+sorted_enclosure_sizes = sort_all(enclosure_sizes)
+print("sorted enclosure by size", sorted_enclosure_sizes)
 
-sorted_supset = sort_subset_by_size_dict(subset,sorted_enclo_sizes)
-print("sorted subset by size",sorted_supset)
+sorted_supset = sort_subset_by_size_dict(subset, sorted_enclosure_sizes)
+print("sorted subset by size", sorted_supset)
 
 theme_zone = create_theme_zone(sorted_supset)
 print(theme_zone)
 
-final_zone = create_solution(distances,enclo_sizes,theme_zone,subset,n)
+final_zone = create_solution(distances, enclosure_sizes, theme_zone, subset, n)
 print(final_zone)
 
 ### for later
-display_zone(final_zone,enclo_sizes) 
+display_zone(final_zone, enclosure_sizes)
 # coordinates = display_zone(theme_zone,subset)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ TP3/algo_v3.py
@@ -0,0 +1,239 @@
+import numpy as np
+
+# enclosure class
+
+
+class Enclosure:
+    def __init__(self, id, size, is_in_subset, out_weights, in_weights, points):
+        self.id = id
+        self.size = size
+        self.is_in_subset = is_in_subset
+        self.out_weights = out_weights
+        self.in_weights = in_weights
+        self.points = points
+
+    def calculateTotalWeight(self):
+        return sum(self.out_weights) + sum(self.in_weights)
+
+    def setStartingPoint(self, visited_points):
+        if len(visited_points) == 0:
+            self.points.append((0, 0))
+            visited_points.append(0, 0)
+        else:
+            origin = (0, 0)
+            placed = False
+            reach = 1
+            dest = (0, 0)
+            while not placed:
+                # Check top
+                if (origin[0], origin[0] + reach) not in visited_points:
+                    dest = (origin[0], origin[1] + reach)
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+                # Check left
+                elif (origin[0] - reach, origin[1]) not in visited_points:
+                    dest = (origin[0] - reach, origin[1])
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+                # Check right
+                elif (origin[0] + reach, origin[1]) not in visited_points:
+                    dest = (origin[0] + reach, origin[1])
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+
+                reach = reach + 1
+
+    def calculateWeightFromEnclosure(self, enclosure):
+        return self.out_weights[enclosure.id] + self.in_weights[enclosure.id]
+
+    def findHeaviestNeighborPossible(self, enclosures, in_subset):
+        selected_neighbor = 0
+        max_weight = 0
+        for enclosure in enclosures:
+            if in_subset and enclosure.is_in_subset and len(enclosure.points) == 0:
+                if self.calculateWeightFromEnclosure(enclosure) > max_weight:
+                    max_weight = self.calculateWeightFromEnclosure(enclosure)
+                    selected_neighbor = enclosure.id
+            elif not in_subset and len(enclosure.points) == 0:
+                if self.calculateWeightFromEnclosure(enclosure) > max_weight:
+                    max_weight = self.calculateWeightFromEnclosure(enclosure)
+                    selected_neighbor = enclosure.id
+        return enclosures[selected_neighbor]
+
+    def grow(self, visited_points):
+        if len(self.points) < self.size:
+            origin = self.points[-1]
+            placed = False
+            dest = (0, 0)
+            while not placed:
+                # Check left
+                if (origin[0] - 1, origin[1]) not in visited_points:
+                    dest = (origin[0] - 1, origin[1])
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+                # Check bottom
+                elif (origin[0], origin[1] - 1) not in visited_points:
+                    dest = (origin[0], origin[1] - 1)
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+                # Check top
+                elif (origin[0], origin[1] + 1) not in visited_points:
+                    dest = (origin[0], origin[1] + 1)
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+                # Check right
+                elif (origin[0] + 1, origin[1]) not in visited_points:
+                    dest = (origin[0] + 1, origin[1])
+                    placed = True
+                    visited_points.append(dest)
+                    self.points.append(dest)
+
+
+# initialise enclosures
+def initialiseEnclosures(subset, sizes, weights):
+    enclosures = []
+    for id in range(0, len(sizes)):
+        is_in_subset = searchInSubset(id, subset)
+        size = sizes[id]
+        out_weights = weights[id]
+        in_weights = []
+        for row in weights:
+            in_weights.append(row[id])
+        enclosures.append(
+            Enclosure(id, size, is_in_subset, out_weights, in_weights, [])
+        )
+    return enclosures
+
+
+# return true if enclosure is in subset
+def searchInSubset(id, subset):
+    for enclosure in subset:
+        if id == enclosure:
+            return True
+    return False
+
+
+# return heaviest enclosure
+def selectFirstEnclosure(enclosures):
+    selected_enclosure = 0
+    max_weight = 0
+    for enclosure in enclosures:
+        if enclosure.is_in_subset:
+            if enclosure.calculateTotalWeight() > max_weight:
+                max_weight = enclosure.calculateTotalWeight()
+                selected_enclosure = enclosure.id
+    return enclosures[selected_enclosure]
+
+
+# read file
+def readFile(file):
+    enclosures_number = 0
+    subset_size = 0
+    max_distance = 0
+    subset = []
+    sizes = []
+    weights = []
+    with open(file, "r") as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            if i == 0:
+                enclosures_number, subset_size, max_distance = map(
+                    int, lines[i].split()
+                )
+            elif i == 1:
+                subset = list(map(int, lines[i].strip().split()))
+            elif i > 1 and i < enclosures_number + 2:
+                index = i - 2
+                sizes.append(int(lines[i].strip()))
+            else:
+                weights = np.resize(weights, (enclosures_number, enclosures_number))
+                index = i - enclosures_number - 2
+                weights[index, :] = lines[i].strip().split()
+    return enclosures_number, subset_size, max_distance, subset, sizes, weights
+
+
+# main
+print()
+print("####################")
+print("READING FILE")
+print("####################")
+print()
+
+enclosures_number, subset_size, max_distance, subset, sizes, weights = readFile(
+    "ex_n10_m5.txt"
+)
+
+print("Enclosure number: " + str(enclosures_number))
+print("Subset size: " + str(subset_size))
+print("Max distance: " + str(max_distance))
+print("Subset: " + str(subset))
+print("Sizes: " + str(sizes))
+print("Weights: " + str(weights))
+
+
+# initialise enclosures
+print()
+print("####################")
+print("INITIALIZING ENCLOSURES")
+print("####################")
+print()
+
+enclosures = initialiseEnclosures(subset, sizes, weights)
+for enclosure in enclosures:
+    print(
+        "Enclosure ID: "
+        + str(enclosure.id)
+        + " in subset ? : "
+        + str(enclosure.is_in_subset)
+    )
+    print("Enclosure Total Weight: " + str(enclosure.calculateTotalWeight()))
+
+# placing enclosures
+print()
+print("####################")
+print("PLACING STARTING POINTS")
+print("####################")
+print()
+
+# select first enclosure and set position
+visited_points = []
+firstEnclosure = selectFirstEnclosure(enclosures)
+firstEnclosure.points.append((0, 0))
+visited_points.append((0, 0))
+
+generated_enclosures = []
+generated_enclosures.append(firstEnclosure)
+
+# select enclosures from subset and set position
+for i in range(0, subset_size - 1):
+    nextEnclosure = firstEnclosure.findHeaviestNeighborPossible(enclosures, True)
+    nextEnclosure.setStartingPoint(visited_points)
+    generated_enclosures.append(nextEnclosure)
+
+# select enclosures from outside subset and set position
+for i in range(0, enclosures_number - subset_size):
+    nextEnclosure = firstEnclosure.findHeaviestNeighborPossible(enclosures, False)
+    nextEnclosure.setStartingPoint(visited_points)
+    generated_enclosures.append(nextEnclosure)
+
+for enclosure in generated_enclosures:
+    print(enclosure.id, enclosure.points)
+
+# grow cells
+for i in range(0, max(sizes)):
+    for enclosure in generated_enclosures:
+        enclosure.grow(visited_points)
+
+
+# write solution
+with open("sol_" + str(enclosures_number) + "_" + str(subset_size) + ".txt", "w") as f:
+    for enclosure in enclosures:
+        for point in enclosure.points:
+            f.write(str(point[0]) + " " + str(point[1]) + " ")
+        f.write("\n")
